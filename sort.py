ac4d4a7cae6ce4188dd234d8b98462754e3c1807
cristianolarrea <junior.larrea.ids@gmail.com>
2023-09-25 12:39:51 -0300

    basis of heapsort implemented

--- sort.py
+++ sort.py
@@ -1,25 +1,50 @@
-def radix_sort(arr: str, n: int, W: int, K: int):
-    contagem = [0]*(K+1)
-    print(contagem)
-    aux = [0]*n
-    for w in range(W-1, -1, -1):
-        print(w)
-        #Seta vetor de contagem com zeros
-        for j in range(K+1):
-            contagem[j] = 0
+def swap(arr, j: int, i: int):
+    arr[i], arr[j] = arr[j], arr[i]
 
-        #Atualizar vetor de contagens com ocorrencias de cada digito
-        for i in range(n):
-            contagem[int(arr[i][w])] += 1
+#RADIX SORT
+def radix_sort(arr, n: int, n_digitos: int, n_range_digitos: int):
+    contagem = [0]*(n_range_digitos+1)
+    aux = [0]*n
+    for digit in range(n_digitos-1, -1, -1):
+
+        #Seta vetor de contagem com zeros
+        for digito in range(n_range_digitos+1):
+            contagem[digito] = 0
+
+        #Atualiza vetor de contagens com ocorrencias de cada digito
+        for elemento in arr:
+            contagem[int(elemento[digit])] += 1
 
         #Vetor acumulados
-        for j in range(1, K+1):
+        for j in range(1, n_range_digitos+1):
             contagem[j] += contagem[j-1]        
 
-        for i in range(n):
-            j = int(arr[i][w])
-            aux[contagem[j]] = arr[i]
+        for elemento in arr:
+            j = int(elemento[digit]) #0 a 9
+            aux[contagem[j]-1] = elemento
             contagem[j]+=1
-        for i in range(n):
-            arr[i] = aux[i]
-    return arr        
+        
+    return aux   
+
+#HEAPSORT
+def heapify(arr, n: int, i: int):
+    idx = i
+    leftIdx = 2*i+1
+    rightIdx = 2*i+2
+    if ((leftIdx<n) and (arr[leftIdx] > arr[idx])):
+        idx = leftIdx
+    if ((rightIdx<n) and (arr[rightIdx] > arr[idx])):
+        idx = rightIdx 
+    if (idx != i):
+        swap(arr, i, idx)
+        heapify(arr, n, idx)       
+    
+def buildHeap(arr, n: int):
+    for i in range(int(n/2 -1), 0, -1):
+        heapify(arr, n, i)
+
+def heapsort(arr, n:int):
+    buildHeap(arr, n) 
+    for i in range(n-1, 1, -1):
+        swap(arr, 0, i)
+        heapify(arr, i, 0)    
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from datagrid import Event
 import time
-from sort import radix_sort
+from sort import radix_sort, heapsort
 
 def calculateTime(startTime):
     total = time.time()-startTime
@@ -29,6 +29,22 @@ def shouldOrderByRadixSortWithSuccess():
     print(sorted)
     print(calculateTime(startTime))    
 
+def shouldOrderByHeapSortWithSuccess():
+    startTime = time.time()
+    
+    #Arrange
+    df = Event()
+    df.owner_id = ['12045', '18492', '09471', '83713', '17387']
+    n = len(df.owner_id)
+
+    #Act
+    heapsort(df.owner_id, n)
+
+    #Assert
+    print(df.owner_id)
+    print(calculateTime(startTime))        
+
 
 #Running tests
-shouldOrderByRadixSortWithSuccess()
+#shouldOrderByRadixSortWithSuccess()
+shouldOrderByHeapSortWithSuccess()
